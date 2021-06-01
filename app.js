b791b2431009919679c53a0253bc813a1959097a
Cheni Chen <celiachenic@gmail.com>
2021-06-01 16:59:09 +0800

    新增向下勻速前進

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ let star = document.querySelector("#star");
 let computedStyle = getComputedStyle(star);
 
 let speedX = 3;
-//let speedY = 3;
+let speedY = 3;
 function move() {
   let currentLeft = parseInt(computedStyle.left); //與視窗左邊距離 (44)
   let currentTop = parseInt(computedStyle.top); //與視窗上方距離 (47)
   let left = currentLeft + speedX;
-
+  let top = currentTop + speedY;
   star.style.left = left + "px";
+  star.style.top = top + "px";
 }
 
 setInterval(() => {
