f6e6c0be06373f8273484c2620fcf0da4c522957
Cheni Chen <celiachenic@gmail.com>
2021-06-01 16:25:11 +0800

    基本環境

---
+++ app.js
@@ -0,0 +1,10 @@
+let star = document.querySelector("#star");
+let style = getComputedStyle(star);
+
+let left = parseInt(style.left); //與視窗左邊距離 44
+console.log(left);
+
+let top = parseInt(style.top);//與視窗上方距離 47
+console.log(top);
+
+
